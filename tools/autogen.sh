28795d25e1c8856cd4815dd85321b3dadd2a6481
Bingyan Liu <wsmlby@gmail.com>
2020-09-25 00:33:03 -0700

    initial

---
+++ tools/autogen.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+# you can either set the environment variables AUTOCONF, AUTOHEADER, AUTOMAKE,
+# ACLOCAL, AUTOPOINT and/or LIBTOOLIZE to the right versions, or leave them
+# unset and get the defaults
+
+git clean -fx
+rm -rf ./src/.deps/
+rm -rf ./src/.libs/
+
+autoreconf --verbose --force --install --make || {
+ echo 'autogen.sh failed';
+ exit 1;
+}
+
+./configure || {
+ echo 'configure failed';
+ exit 1;
+}
+
+echo
+echo "Now type 'make' to compile this module."
+echo
+
+make
+sudo make install
